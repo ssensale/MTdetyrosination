6030fb90c53d7bcf5cbd0713b35c8f6017d1abb7
ssensale <62820740+ssensale@users.noreply.github.com>
2023-02-03 03:00:43 -0500

    Delete plot_tubule.m

--- plot_tubule.m
+++
@@ -1,8 +0,0 @@
-function plot_tubule = plot_tubule(center_rad, cell_rad,cylinder_locations,tubule_size,discretization,modified,growing)
-
-for i=1:size(cylinder_locations,1)
-    plot_tubule_color(cylinder_locations(i,:),tubule_size,modified{i},growing(i),discretization);
-    hold on
-end;
-view(3)
-end
