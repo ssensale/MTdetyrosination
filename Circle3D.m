1b2172ab6a99628890353bfee5853247e9c5eba7
ssensale <62820740+ssensale@users.noreply.github.com>
2023-02-03 02:55:24 -0500

    Delete Circle3D.m

--- Circle3D.m
+++
@@ -1,5 +0,0 @@
-function points = Circle3D(center,normal,radius)
-    theta=0:0.01:2*pi;
-    v=null(normal);
-    points=repmat(center',1,size(theta,2))+radius*(v(:,1)*cos(theta)+v(:,2)*sin(theta));
-end
